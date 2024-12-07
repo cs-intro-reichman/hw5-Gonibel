0f122f601e4a64c596413106c329f6e63e70fc61
Gonibel <gbelkine@gmail.com>
2024-12-07 17:08:33 +0200

    HW5 update 6

--- MyString.java
+++ MyString.java
@@ -73,10 +73,9 @@ public class MyString {
      */
     public static String spacedString(String str) {
         //// Replace the following statement with your code
-        if(str.length() == 0) {return " ";}
+        if(str.length() == 0) {return "";}
         if(str.length() == 1) {
-            String newstr = " " + str;
-            return newstr;
+            return str;
         }
         String newstr = "";
         for(int i = 0; i < str.length(); i++) {
