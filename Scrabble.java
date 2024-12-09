589894cca146b797a06bf99b4d69753ade129948
Gonibel <gbelkine@gmail.com>
2024-12-09 12:44:12 +0200

    HW5 update 16

--- Scrabble.java
+++ Scrabble.java
@@ -104,7 +104,7 @@ public class Scrabble {
 		In in = new In();
 		while (hand.length() > 0) {
 			System.out.println("Current Hand: " + MyString.spacedString(hand));
-			System.out.println("Enter a word, or '.' to finish playing this hand:");
+			System.out.print("Enter a word, or '.' to finish playing this hand: ");
 			// Reads the next "token" from the keyboard. A token is defined as a string of 
 			// non-whitespace characters. Whitespace is either space characters, or  
 			// end-of-line characters.
